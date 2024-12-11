7ad484ac5dd0ce8d0789e4c9ee6639bc21458acf
Tjebbe Vlieg <tvlieg@eco-movement.com>
2024-12-11 11:53:21 +0100

    2024 day11 2

--- 2024/day11/main.go
+++ 2024/day11/main.go
@@ -1,57 +1,69 @@
 package main
 
 import (
-	"bufio"
 	"fmt"
 	"math"
-	"os"
 	"strconv"
 	"strings"
 )
 
 func main() {
-	// filePath := "example_input"
-	filePath := "input"
+	const (
+		example = "125 17"
+		input   = "554735 45401 8434 0 188 7487525 77 7"
+	)
+	fmt.Println("Example:", run(parse(example), 6))
+	fmt.Println("Part 1:", run(parse(input), 25))
+	fmt.Println("Part 2:", run(parse(input), 75))
+}
 
-	file, _ := os.Open(filePath)
-	defer file.Close()
-
-	scanner := bufio.NewScanner(file)
-	scanner.Scan()
-	line := scanner.Text()
-
-	var pebbles []int
-	for _, s := range strings.Split(line, " ") {
+func parse(s string) map[int]int {
+	pebbles := make(map[int]int)
+	for _, s := range strings.Split(s, " ") {
 		n, _ := strconv.Atoi(s)
-		pebbles = append(pebbles, n)
+		pebbles[n] = 1 // Asume no duplicates in input
+	}
+	return pebbles
+}
+
+func run(pebbles map[int]int, n int) (count int) {
+	for range n {
+		pebbles = blink(pebbles)
 	}
 
-	for range 25 {
-		var nextPebbles []int
-		for _, p := range pebbles {
-			switch {
-			case p == 0:
-				nextPebbles = append(nextPebbles, 1)
-			case (numDigits(p) % 2) == 0:
-				nextPebbles = append(nextPebbles, split(p)...)
-			default:
-				nextPebbles = append(nextPebbles, p*2024)
-			}
-		}
-		pebbles = nextPebbles
+	for _, c := range pebbles {
+		count += c
 	}
-	fmt.Println("Part 1:", len(pebbles))
+
+	return
+}
+
+func blink(pebbles map[int]int) map[int]int {
+	res := make(map[int]int, len(pebbles))
+	for pebble, count := range pebbles {
+		switch {
+		case pebble == 0:
+			res[1] += count
+		case (numDigits(pebble) % 2) == 0:
+			spl := split(pebble)
+			res[spl[0]] += count
+			res[spl[1]] += count
+		default:
+			res[pebble*2024] += count
+		}
+	}
+	return res
 }
 
 func numDigits(n int) int {
 	return int(math.Floor(math.Log10(float64(n)) + 1))
 }
 
-func split(p int) []int {
-	d := numDigits(p)
+func split(pebble int) [2]int {
+	d := numDigits(pebble)
 	f := int(math.Pow10(d / 2))
-	return []int{
-		p / f,
-		p % f,
+	return [2]int{
+		pebble / f,
+		pebble % f,
 	}
 }
