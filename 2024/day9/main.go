8765ac42575287b12efba2c627f82f05d2c42948
Tjebbe Vlieg <tvlieg@eco-movement.com>
2024-12-14 21:58:40 +0100

    2024 day9 2

--- 2024/day9/main.go
+++ 2024/day9/main.go
@@ -10,13 +10,17 @@ func main() {
 	// filePath := "example_input"
 	filePath := "input"
 
-	const free = -1
+	fmt.Println("Part 1:", part1(filePath))
+	fmt.Println("Part 2:", part2(filePath))
+}
 
-	file, _ := os.Open(filePath)
+func part1(fp string) int {
+	file, _ := os.Open(fp)
 	defer file.Close()
 
 	scanner := bufio.NewScanner(file)
 
+	const free = -1
 	var blocks []int
 	scanner.Scan()
 	for i, r := range scanner.Text() {
@@ -49,5 +53,60 @@ func main() {
 		i++
 		blocks = blocks[1:]
 	}
-	fmt.Println("Part1:", sum)
+	return sum
+}
+
+func part2(fp string) int {
+	f, _ := os.Open(fp)
+	defer f.Close()
+
+	scanner := bufio.NewScanner(f)
+	scanner.Scan()
+	var (
+		files  []*file
+		spaces []*space
+		cursor int
+	)
+	for i, r := range scanner.Text() {
+		n := int(r - '0')
+
+		if i%2 == 0 {
+			id := i / 2
+			files = append(files, &file{id: id, pos: cursor, size: n})
+		} else {
+			spaces = append(spaces, &space{pos: cursor, size: n})
+		}
+
+		cursor += n
+	}
+
+	compacted := make([]*file, 0, len(files))
+	for i := len(files) - 1; i >= 0; i-- {
+		f := files[i]
+		for _, s := range spaces {
+			if f.size <= s.size && s.pos < f.pos {
+				f.pos = s.pos
+				s.pos += f.size
+				s.size -= f.size
+				break
+			}
+		}
+		compacted = append(compacted, f)
+	}
+
+	var sum int
+	for _, f := range compacted {
+		for i := range f.size {
+			sum += f.id * (f.pos + i)
+		}
+	}
+	return sum
+}
+
+type file struct {
+	id, pos, size int
+}
+
+type space struct {
+	pos, size int
 }
